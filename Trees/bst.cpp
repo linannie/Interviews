2be341da97e16f7de4e4ac2831ad23309c5d3c76
Annie  Lin <https://github.com/linannie/linannie.github.io/commit/7c494e04c2cf7492290aecd4a064ccb7e3416ee6.patch>
2018-03-03 21:43:22 -0800

    traversals

--- Trees/bst.cpp
+++ Trees/bst.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 class Node{
-  private:
+  public:
     int data;
     Node* right;
     Node* left;
-  public: 
+  //public: 
     Node();
     ~Node();
     Node* newNode(int data);
@@ -35,23 +35,66 @@ Node* Node::newNode(int data){
 
 Node* Node::insert(Node* root, int data){
   if(root == NULL){                           // if there's no root
-    Node* node = newNode(data);               // create a new node!
-    return node;
+    return newNode(data);               // create a new node!
   }
-  else{
-    if (data < root->data){                   // if the data is less than root data, go left
-      root->left = newNode(data); 
-    }
-    else{
-      root->right = newNode(data);            // if the data is more than root data, go right
-    }
+  if (data < root->data){                   // if the data is less than root data, go left
+    root->left = insert(root->left, data); 
+  }
+  if (data > root->data){
+    root->right = insert(root->right, data);  // if the data is more than root data, go right
+  }
 
-  } 
+   
   return root;
 }
 
+/////////// TRAVERSALS ////////////////
+void preorder(Node* node){
+  if(node != NULL){
+    cout << node->data << " ";
+    preorder(node->left);
+    preorder(node->right);
+  }
+}
+
+void inorder(Node* node){                   // Left, data, Right
+  if(node != NULL){
+    inorder(node->left);
+    cout << node->data << " ";
+    inorder(node->right);
+  }
+}
+
+void postorder(Node* node){
+  if(node != NULL){
+    postorder(node->left);
+    postorder(node->right);
+    cout << node->data << " ";
+  }
+}
+
+///////////// SEARCHES ////////////
 
 int main(){
+  Node* root = root->newNode(50);                           
+  root->insert(root, 20);                        
+  root->insert(root, 69);
+  root->insert(root, 70);
+  root->insert(root, 13);
+  root->insert(root, 23);
+  root->insert(root, 56);
   
+
+  cout << "INORDER: ";
+  inorder(root);
+  cout << endl;
+
+  cout << "PREORDER: ";
+  preorder(root);
+  cout << endl;
+ 
+  cout << "POSTORDER: ";
+  postorder(root);
+  cout << endl;
   return 0;
 }
